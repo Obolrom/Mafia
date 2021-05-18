bcb02fe9f84bc6eb44c038d7dae6bef980eeccee
Obolrom <obolrom@gmail.com>
2021-05-18 16:50:38 +0300

    unit test

--- automatatest.py
+++ automatatest.py
@@ -8,6 +8,9 @@ class MyTestCase(unittest.TestCase):
     def test_something2(self):
         self.assertTrue(6 == 6)
 
+    def testStrings(self):
+        self.assertFalse("good" == "bad")
+
 
 if __name__ == '__main__':
     unittest.main()
