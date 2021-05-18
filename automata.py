584e1b490feed69a37aef6476f2d15499e296404
IvanGroza <ivangroza2000@gmail.com>
2021-05-18 17:36:08 +0300

    feat: player subclasses

--- automata.py
+++ automata.py
@@ -10,5 +10,44 @@ class Player(ABC):
         pass
 
 
+class Citizen(Player):
+    def __init__(self, nickname):
+        super(Citizen, self).__init__(nickname)
+
+    def talk_time(self):
+        pass
+
+
+class Sheriff(Citizen):
+    def __init__(self, nickname):
+        super(Sheriff, self).__init__(nickname)
+
+    def mafia_check(self, nickname):
+        pass
+
+
+class Mafia(Player):
+    def __init__(self, nickname):
+        super(Mafia, self).__init__(nickname)
+
+    def talk_time(self):
+        pass
+
+    def kill_player(self, nickname):
+        pass
+
+
+class Godfather(Mafia):
+    def __init__(self, nickname):
+        super(Godfather, self).__init__(nickname)
+
+    def sheriffs_check(self, nickname):
+        pass
+
+
 if __name__ == '__main__':
-    player = Player()
+    players = [Mafia(""), Godfather(""), Citizen(""), Citizen(""), Citizen(""), Mafia(""), Sheriff("")]
+    for player in players:
+        if player is Mafia:
+            player.kill_player("")
+
